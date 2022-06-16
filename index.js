a520fba08428e4215c235caf163103b9ea1de4d8
Krish Vijayvergia <krishwinrocks@gmail.com>
2022-06-16 14:27:24 -0500

    added the env file and did slight configuration for mongoose

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // wassup guys
+const { Client, Intents } = require('discord.js');
+require("dotenv").config()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const mongoose = require('mongoose')
+
+client.on('ready!!!', async () => {
+    await mongoose.connect(process.env.MONGO_URI,
+        {
+            keepAlive: true
+        })
+}),
+    
+
+    client.login(process.env.DISCORD_TOKEN);
