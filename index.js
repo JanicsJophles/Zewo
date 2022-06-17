62703a576da9572538a7336fa2fc75daa3ef9122
Krish Vijayvergia <krishwinrocks@gmail.com>
2022-06-16 23:03:49 -0500

    cleaned up all the code now it is ready for configuration

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 // wassup guys
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9")
+const { Client, Intents, Collection } = require('discord.js');
 require("dotenv").config()
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { readdirSync } = require('node:fs');
+//command handler below
 
-client.on('ready!!!', async () => {
-    await mongoose.connect(process.env.MONGO_URI,
-        {
-            keepAlive: true
-        })
+
+
+//doing this later :D
+
+
+
+//end of cmd handler
+
+//start of mongoose
+
+
+client.on('ready', async () => {
+       console.log("woot woot its up!")
 }),
     
 
-    client.login(process.env.DISCORD_TOKEN);
+
+
+
+
+    client.login(process.env.DISCORD_TOKEN)
+    //.then(() => mongoose.connect(process.env.MONGO_URI));
