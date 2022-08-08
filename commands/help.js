6b692d30a4fe22ad9ba6a3791d531c32b8a83de6
Krish Vijayvergia <krishwinrocks@gmail.com>
2022-08-08 14:58:36 -0500

    edited emojis

--- commands/help.js
+++ commands/help.js
@@ -7,24 +7,20 @@ module.exports = {
     async execute(interaction){
         const randomSideColors = ["#0099ff","#FF0000","#FF7F00","#FFFF00","#00FF00","#8B00FF"]
         const randomSideColor = randomSideColors[Math.floor(Math.random() * randomSideColors.length)];
-        const emojiOne = interaction.guild.emojis.cache.find(emoji => emoji.name === '123egirls');
-        const emojiTwo = interaction.guild.emojis.cache.find(emoji => emoji.name === 'egirlithink');
-        const emojiThree = interaction.guild.emojis.cache.find(emoji => emoji.name === 'egirlsa');
-        const emojiFour = interaction.guild.emojis.cache.find(emoji => emoji.name === 'emoji_11');
-        const emojiFive = interaction.guild.emojis.cache.find(emoji => emoji.name === 'coochieworld');
+        
         const embed = new EmbedBuilder()
         .setAuthor({name: `Help command for Zewo`})
         .setTitle('Shows all the commands :D')
         .addFields(
-            {name: `${emojiOne} Text Commands!!`, value: "`sheen`, `bitches`\n" },
+            {name: `<a:123egirls:1004192267784953866> Text Commands!!`, value: "`sheen`, `bitches`\n" },
             
-            {name: `${emojiTwo} Image Commands!!`, value: "`8ball`, `randomcat`, `define`, `hotdog`, `come`, `poll`, `coinflip`\n"},
+            {name: `<a:egirlithink:1004192210671116338> Image Commands!!`, value: "`8ball`, `randomcat`, `define`, `hotdog`, `come`, `poll`, `coinflip`\n"},
             
-            {name: `${emojiThree} Rating Commands!!`, value: "`gayrate`, `funnyrate`, `pp`, `clownrate`, `waifurate`\n"},
+            {name: `<a:egirlsa:1004192313926484048> Rating Commands!!`, value: "`gayrate`, `funnyrate`, `pp`, `clownrate`, `waifurate`\n"},
             
-            {name: `${emojiFour} Interactive Commands ;)`, value: "`slap`, `kiss`, `ship`, `hug`\n"},
+            {name: `<:emoji_11:1003136058428493854> Interactive Commands ;)`, value: "`slap`, `kiss`, `ship`, `hug`\n"},
 
-            {name: `${emojiFive} Info Commands :D`, value: "`serverinfo`, `avatar`"}
+            {name: `<:coochieworld:1003132134195736676> Info Commands :D`, value: "`serverinfo`, `avatar`"}
 
         )
         .setColor(randomSideColor)
