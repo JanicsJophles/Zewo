aae5f92f6bef57fa4d010b661e5707cf45fe1261
BuildTools <unconfigured@null.spigotmc.org>
2022-08-14 01:17:34 -0500

    finished memberinfo cmd

--- commands/memberinfoo.js
+++ commands/memberinfoo.js
@@ -6,11 +6,22 @@ module.exports = {
     .setDescription('shows info about a member of the server')
     .addUserOption(options => options.setName('target').setDescription('select a user (optional)').setRequired(false)),
     async execute(interaction){
+        //member not mentioned start
         const dumbBalls = interaction.user
         const dumb = interaction.member
-        const mentionedMember = interaction.options.getUser('target')
         const roles = dumb._roles
         const mentionRoles = roles.map(r => `<@&${r}>`)
+        const roleees = (mentionRoles == '') ? 'no roles' : mentionRoles
+        const nameOfNick = dumb.nickname
+        const nick = (nameOfNick == 'null') ? 'no nickname' : nameOfNick
+
+        //end 
+
+
+        
+        
+
+        
 
         const embed =  new EmbedBuilder()
         .setAuthor({name: `Info for ${dumbBalls.tag}`, 
@@ -18,16 +29,43 @@ module.exports = {
                     
                     })
         .addFields(
-            {name: `Nickname:`, value: `${dumb.nickname}`},
-            {name: `ID:`, value: `${dumbBalls.id}`, inline: true},
-            {name: `Is a bot?`, value: `${dumbBalls.bot}`, inline:true},
-            {name: `Member Created At:`, value: `${dumbBalls.createdAt}`},
-            {name: `Member Joined At:`, value: `${dumb.joinedAt}`,},
-            {name: `Members Roles:`, value: `${mentionRoles}`},
+            {name: `Nickname:`, value: `${nick}`},
+            {name: `ID:`, value: `${dumbBalls.id}`},
+             {name: `Member Created At:`, value: `${dumbBalls.createdAt}`},
+             {name: `Member Joined At:`, value: `${dumb.joinedAt}`,},
+             {name: `Members Roles:`, value: `${roleees}`},
         )
 
         
-        await interaction.reply({embeds: [embed]})
+        if(interaction.options.getMember('target') || interaction.options.getUser('target') == true){
+            const menMember = interaction.options.getMember('target')
+        const mentionedMember = interaction.options.getUser('target')
+        const menRoles = menMember._roles
+        const menMemberRoles = menRoles.map(r => `<@&${r}>`)
+        const menMsenRoles = (menMemberRoles == '') ? 'no roles' : menMemberRoles
+
+        const embedTwo = new EmbedBuilder()
+        .setAuthor({name: `Info for ${mentionedMember.username}`, 
+                    iconURL: `${mentionedMember.displayAvatarURL({format: `png`})}`, 
+                    
+                    })
+        .addFields(
+            {name: `Nickname:`, value: `${menMember.nickname}`},
+            {name: `ID:`, value: `${menMember.id}`},
+             {name: `Member Created At:`, value: `${mentionedMember.createdAt}`},
+             {name: `Member Joined At:`, value: `${menMember.joinedAt}`,},
+             {name: `Members Roles:`, value: `${menMsenRoles}`},
+        )
+
+
+            await interaction.reply({embeds: [embedTwo]})
+        }   else    {
+            await interaction.reply({embeds: [embed]})
+        }
+        
+        
+
+        
         
     }
 
--- ownerCommands/botInfo.js
+++ ownerCommands/botInfo.js
@@ -2,6 +2,7 @@ const {
     SlashCommandBuilder,
     PermissionFlagsBits,
     PermissionsBitField,
+    EmbedBuilder,
  } = require('discord.js')
 
 module.exports = {
@@ -12,11 +13,18 @@ module.exports = {
     async execute(interaction, client){
     let servers = client.guilds.cache.size
     let serverNames = client.guilds.cache.map(g => g.name)
+    
     const {roles} = interaction.member
     const role = await interaction.guild.roles.fetch('994458947798978668')
-    
+    const embed = new EmbedBuilder()
+    .setTitle("Bot Info")
+    .setAuthor({name: `Bot Name: Zewo`})
+    .setFields(
+        {name: `Total Servers:`, value: `${servers}`},
+        {name: `Server Names:`, value: `${serverNames}`}
+    )
     if (roles.cache.has('994458947798978668')){
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}. There are ${servers} servers that have this bot! The server names are ${serverNames}`);
+        await interaction.reply({embeds: [embed]});
     } else {
         await interaction.reply('you cant use this cmd!!')
     }
